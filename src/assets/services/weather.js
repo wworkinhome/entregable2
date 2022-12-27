faa66b3f67640c10e018ac28c3f7fc33cfa64757
Wilmer Antonio Mosquera Figueroa <114489499+wworkinhome@users.noreply.github.com>
2022-12-27 17:21:28 -0500

    Update weather.js

--- src/assets/services/weather.js
+++ src/assets/services/weather.js
@@ -3,7 +3,7 @@ import { ajax } from "../tools/ajax";
 export const getCityWeather = async city => {
     const optionsRequest = {
         method: "GET",
-        url: "http://api.openweathermap.org/data/2.5/weather",
+        url: "https://api.openweathermap.org/data/2.5/weather",
         params: {
             q: city,
             appid: "21e3195e9bfeb6879e788ec605b09ab0",
